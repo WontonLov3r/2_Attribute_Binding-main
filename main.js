177ab5f8993ddd075106f86e193ee58070a59d83
WontonLov3r <danielhariantowu@gmail.com>
2024-04-30 11:15:02 -0400

    finished vue 2 with link

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 const app = Vue.createApp({
   data () {
     return {
-      product: 'Socks'
-      image: './assets/images/socks_green.jpg'
+      product: 'Socks',
+      image: '/assets/images/socks_blue.jpg', 
+      URL: 'https://ondo.com/?utm_source=google&utm_medium=cpc&utm_campaign=gdn_disco_demand-den_prospecting_tof|Cj0KCQjwir2xBhC_ARIsAMTXk87x3vSAEvzNivlIzkwLKObFQLwp5bmnrVVC2JvkpELfHkV5XBdVMPgaAjczEALw_wcB&gclid=Cj0KCQjwir2xBhC_ARIsAMTXk87x3vSAEvzNivlIzkwLKObFQLwp5bmnrVVC2JvkpELfHkV5XBdVMPgaAjczEALw_wcB'
     }
   }
 })
